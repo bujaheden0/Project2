848c84e2db81b3f5b605355c2c008bce573ec19e
woody <tanawut913@gmail.com>
2018-02-18 20:37:26 +0700

    update change local v1

---
+++ src/app/map/map.ts
@@ -0,0 +1,6 @@
+export class Location {
+    id: number;
+    name: string;
+    Lat: number;
+    Lng:number;
+  }
---
+++ src/app/map/mock-locationlist.ts
@@ -0,0 +1,6 @@
+import { Location } from './map';
+
+export const LOCATIONS: Location[] = [
+    { id: 1, name: 'อำเภอกะทู้', Lat: 7.917431 ,Lng:98.313611},
+    { id: 2, name: 'อำเภอเมืองภูเก็ต', Lat: 7.880342 ,Lng:98.386420}
+];
