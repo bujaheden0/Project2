851b27e0af46d730a887c82d6fc3be7cb40d4750
woody <tanawut913@gmail.com>
2018-04-21 18:38:28 +0700

    update serach map by distance no currrent position

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -85,25 +85,25 @@ export class MapComponent implements OnInit {
       }
       infoWindow = new google.maps.InfoWindow;
       // Try HTML5 geolocation.
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-          var pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          };
+      // if (navigator.geolocation) {
+      //   navigator.geolocation.getCurrentPosition(function (position) {
+      //     var pos = {
+      //       lat: position.coords.latitude,
+      //       lng: position.coords.longitude
+      //     };
 
-          infoWindow.setPosition(pos);
-          infoWindow.setContent('คุณอยู่ที่นี่');
-          infoWindow.open(map);
-          map.setCenter(pos);
-          map.setZoom(15);
-        }, function () {
-          this.handleLocationError(true, infoWindow, map.getCenter());
-        });
-      } else {
-        // Browser doesn't support Geolocation
-        this.handleLocationError(false, infoWindow, map.getCenter());
-      }
+      //     infoWindow.setPosition(pos);
+      //     infoWindow.setContent('คุณอยู่ที่นี่');
+      //     infoWindow.open(map);
+      //     map.setCenter(pos);
+      //     map.setZoom(15);
+      //   }, function () {
+      //     this.handleLocationError(true, infoWindow, map.getCenter());
+      //   });
+      // } else {
+      //   // Browser doesn't support Geolocation
+      //   this.handleLocationError(false, infoWindow, map.getCenter());
+      // }
     })
   }
 
@@ -311,7 +311,7 @@ export class MapComponent implements OnInit {
       this.Dorm = res;
       var obj = Object.keys(this.Dorm).length;
 
-      console.log(this.Dorm)
+      // console.log(this.Dorm)
       var radius1 = this.Radius;
       var radius_circle;
       var markers_on_map = [];
@@ -328,83 +328,63 @@ export class MapComponent implements OnInit {
       });
       var infoWindow = new google.maps.InfoWindow;
       geocoder = new google.maps.Geocoder();
-      if (navigator.geolocation) {
-        // var pos1 = { lat: 7.894866, lng: 98.352092 };
+      var mylocation = new google.maps.LatLng(7.894866, 98.352092);
 
-        navigator.geolocation.getCurrentPosition(function (position) {
+      radius_circle = new google.maps.Circle({
+        center: mylocation,
+        radius: radius1 * 1000,
+        clickable: false,
+        map: map
+      });
 
-          var pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          };
-
-          infoWindow.setPosition(pos);
-          infoWindow.setContent('คุณอยู่ที่นี่');
-          infoWindow.open(map);
-          map.setCenter(pos);
-          map.setZoom(13);
-
-          radius_circle = new google.maps.Circle({
-            center: pos,
-            radius: radius1 * 1000,
-            clickable: false,
-            map: map
+      if (radius_circle) map.fitBounds(radius_circle.getBounds());
+      for (var i = 0; i < obj; i++) {
+        //  var pos1 = { lat: 7.894866, lng: 98.352092 };
+        // console.log(dorm[0])
+        var mylocation = new google.maps.LatLng(7.894866, 98.352092);
+        var myLatLng1 = new google.maps.LatLng(dorm[i].lat, dorm[i].long);
+        var myLatLng = { lat: dorm[i].lat, lng: dorm[i].long };
+        // var marker_lat_lng = new google.maps.LatLng(location.lat, location.lng);
+        var distance_from_location = google.maps.geometry.spherical.computeDistanceBetween(mylocation, myLatLng1); //distance in meters between your location and the marker
+        if (distance_from_location <= radius1 * 1000) {
+          var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            animation: google.maps.Animation.DROP,
+            title: 'hello world'
           });
 
-          if (radius_circle) map.fitBounds(radius_circle.getBounds());
-          for (var i = 0; i < obj; i++) {
-            //  var pos1 = { lat: 7.894866, lng: 98.352092 };
-            // console.log(dorm[0])
-            var mylocation = new google.maps.LatLng(7.894866, 98.352092);
-            var myLatLng1 = new google.maps.LatLng(dorm[i].lat, dorm[i].long);
-            var myLatLng = { lat: dorm[i].lat, lng: dorm[i].long };
-            // var marker_lat_lng = new google.maps.LatLng(location.lat, location.lng);
-            var distance_from_location = google.maps.geometry.spherical.computeDistanceBetween(mylocation, myLatLng1); //distance in meters between your location and the marker
-            if (distance_from_location <= radius1 * 1000) {
-              var marker = new google.maps.Marker({
-                position: myLatLng,
-                map: map,
-                animation: google.maps.Animation.DROP,
-                title: 'hello world'
-              });
+          contentString = '<div id="content">' +
+            '<div id="siteNotice">' +
+            '</div>' +
+            '<h2 id="firstHeading" class="firstHeading">' + dorm[i].name + '</h2>' +
+            '<div id="bodyContent">' +
+            '<p><h5>ที่อยู่: </h5>' + dorm[i].address +
+            '<h5>เบอร์โทรศัพท์: </h5>' + dorm[i].tel +
+            '<h5>ยูนิตไฟฟ้า: </h5>' + dorm[i].electric_unit +
+            '<h5>ค่าน้ำ: </h5>' + dorm[i].water_bill +
+            '<h5>ราคาห้องพัดลม: </h5>' + dorm[i].price.fan_price +
+            '<h5>ราคาห้องแอร์: </h5>' + dorm[i].price.air_price +
+            '<h5>คำอธิบาย: </h5>' + dorm[i].description +
+            '<h5>ประเภท: </h5>' + dorm[i].type + '</p>' +
+            '</div>' +
+            '</div>';
 
-              contentString = '<div id="content">' +
-                '<div id="siteNotice">' +
-                '</div>' +
-                '<h2 id="firstHeading" class="firstHeading">' + dorm[i].name + '</h2>' +
-                '<div id="bodyContent">' +
-                '<p><h5>ที่อยู่: </h5>' + dorm[i].address +
-                '<h5>เบอร์โทรศัพท์: </h5>' + dorm[i].tel +
-                '<h5>ยูนิตไฟฟ้า: </h5>' + dorm[i].electric_unit +
-                '<h5>ค่าน้ำ: </h5>' + dorm[i].water_bill +
-                '<h5>ราคาห้องพัดลม: </h5>' + dorm[i].price.fan_price +
-                '<h5>ราคาห้องแอร์: </h5>' + dorm[i].price.air_price +
-                '<h5>คำอธิบาย: </h5>' + dorm[i].description +
-                '<h5>ประเภท: </h5>' + dorm[i].type + '</p>' +
-                '</div>' +
-                '</div>';
-
-              contentStrings.push(contentString);
-              var infowindow = new google.maps.InfoWindow({
-                maxWidth: 200
-              });
-              google.maps.event.addListener(marker, 'click', (function (marker, i) {
-                return function () {
-                  infowindow.setContent(contentStrings[i]);
-                  infowindow.open(map, marker);
-                }
-              })(marker, i));
+          contentStrings.push(contentString);
+          infowindow = new google.maps.InfoWindow({
+            maxWidth: 200
+          });
+          google.maps.event.addListener(marker, 'click', (function (marker, i) {
+            return function () {
+              infowindow.setContent(contentStrings[i]);
+              infowindow.open(map, marker);
             }
+          })(marker, i));
+        }
 
-          }
-
-        }, function () {
-          this.handleLocationError(true, infoWindow, map.getCenter());
-        });
-      } else {
-        // Browser doesn't support Geolocation
-        this.handleLocationError(false, infoWindow, map.getCenter());
       }
+
+
       // });// service get dorm
     });
   }
